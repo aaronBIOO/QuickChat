548ac2d530106c022831f66ae794adf224305f9a
aaronBIOO <aaronnabioo@gmail.com>
2025-10-11 14:13:40 +0000

    fix: clean up

--- server/src/routes/webhookRoute.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { handleClerkWebhook } from "@/controllers/webhookController.js";
-
-const router = express.Router();
-
-router.post("/webhooks/clerk", express.raw({ type: "application/json" }), handleClerkWebhook);
-
-export default router;
